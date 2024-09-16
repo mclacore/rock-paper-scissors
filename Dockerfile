81b554a09458d960a5e694371116c385fae8407f
mclacore <mclacore@gmail.com>
2024-09-16 11:43:28 -0700

    cleaning up repo, ready for new webinar

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,6 @@ WORKDIR /app
 
 RUN pip install -r requirements.txt
 
-EXPOSE 5000
+EXPOSE 8000
 
 CMD ["python", "rps.py"]
--- massdriver/src/main.tf
+++
@@ -1,8 +0,0 @@
-module "helm" {
-  source                 = "github.com/massdriver-cloud/terraform-modules//massdriver-application-helm?ref=c09e929"
-  name                   = var.md_metadata.name_prefix
-  namespace              = var.namespace
-  chart                  = "${path.module}/chart"
-  kubernetes_cluster     = var.kubernetes_cluster
-  helm_additional_values = {}
-}
